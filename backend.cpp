246b40e8c6c6d561bcceaaf2860d7e61eb69c9e0
Swung0x48 <1339148162@qq.com>
2019-12-17 09:40:49 +0800

    Finished isISBNCorrect()

--- backend.cpp
+++ backend.cpp
@@ -3,20 +3,75 @@
 //
 
 #include "backend.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 struct Date
 {
     int year;
     int month;
     int day;
 };
-typedef Date date;
 
 struct Book
 {
+    int num;
     char ISBN[20];
     char name[20];
     char author[20];
-    date;
+    struct Date;
     double price;
-    struct Book * next;
+    struct Book *next;
 };
+
+int isISBNCorrect(char ISBN[20], int WillCorrect)
+{
+    int ISBN_digit[20];
+
+    for (int i = 0, j = 0; i < 13; i++)
+    {
+
+        if (ISBN[i] == '-');
+        else if (ISBN[i] == 'X')
+            ISBN_digit[j] = 'X';
+        else
+        {
+            ISBN_digit[j] = ISBN[i] - '0';
+            j++;
+        }
+    }
+
+    long long sum = 0;
+    int checksum = 0;
+    for (int i = 0; i < 9; i++)
+    {
+        sum += ISBN_digit[i] * (i + 1);
+
+    }
+
+    checksum = sum % 11;
+    if (checksum == ISBN_digit[9])
+        return 1;
+    else if (checksum == 10 && ISBN_digit[9] == 'X')
+        return 1;
+    else
+    {
+        if (WillCorrect == 1)
+        {
+            if (checksum != 10)
+                ISBN[12] = checksum + '0';
+            else
+                ISBN[12] = 'X';
+
+            return 0;
+        }
+        else
+            return 0;
+    }
+
+    return 2;
+}
+
+
+
+
