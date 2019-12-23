22465286169fdcde08f614541939a82d52858d4d
Swung0x48 <1339148162@qq.com>
2019-12-23 22:12:16 +0800

    Updated OutputList()

--- ui.cpp
+++ ui.cpp
@@ -7,3 +7,15 @@ void print_info()
 	printf("+ ——————————————————————————————————+\n");
 	printf("1 列出所有图书\n2 查询指定图书\n3 添加记录\n4 删除记录\n5 清空图书列表\n6 从文件读取列表/写入当前列表到文件\n0 退出\n\n请选择：");
 }
+
+void OutputLabel()
+{
+    printf("Index       ISBN       name    Author    YYYY/MM/DD    Price\n");
+}
+
+void OutputPagePrompt()
+{
+    printf("Press w for previous page.\n");
+    printf("Press s for next page.\n");
+    printf("Press any other key to return to menu.\n");
+}
