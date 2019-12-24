b0753819c0a70db4841e639d5b14e1740a123a32
823082149 <823082149@qq.com>
2019-12-24 22:28:59 +0800

    File IO completed

--- file_io.cpp
+++ file_io.cpp
@@ -2,4 +2,29 @@
 // Created by Swung 0x48 on 2019/12/16.
 //
 
-#include "file_io.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+FILE *WRITE(struct Book *p)
+{
+    FILE *fp;
+    if((fp=fopen("Bookdata.txt","wb"))==NULL)
+    {
+        printf("cannot open file\n");
+        return NULL;
+    }
+    return fp;
+}
+
+
+FILE *READ()
+{
+    FILE *fp;
+    struct Book *head;
+    if((fp=fopen("Bookdata.txt","wb"))==NULL)
+    {
+        printf("cannot open file\n");
+        exit(0);
+    }
+    return fp;
+}
